9cb3bd4d71b89c052851bf6500dd0c01d2850350
1Vevner <Shadowshakya@gmail.com>
2023-01-02 17:43:27 +0545

    back at it again

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,5 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import Rend from './rend'
+
+let counter = 1
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root_two')).render(<Rend counter = {counter}/>)
